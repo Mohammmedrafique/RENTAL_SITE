e0cc9710e3c001bda5f7ea34a6d588cf2c415352
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2024-02-16 21:53:41 +0530

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,7 +40,7 @@ function App() {
         <h1 className="text-3xl font-semibold text-center my-8">
           Checkout Our Product
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {products.map((product) => (
             <Product
               key={product.id}
